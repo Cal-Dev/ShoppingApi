a553f199ff556a02cf74f72676d5d77316d67d49
Cal-Dev <31552633+Cal-Dev@users.noreply.github.com>
2019-05-21 01:19:44 -0500

    ready to test out api/products

---
+++ ShoppingApi/Controllers/ProductsController.cs
@@ -0,0 +1,29 @@
+﻿using ShoppingApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ShoppingApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Products")]
+    public class ProductsController : Controller
+    {
+        Product[] products = new Product[]
+        {
+            new Product { Id = 1 , Name = "Mechanical Keyboard", Category="Electronics", Price = 60 },
+           new Product  { Id = 2 , Name = "Legendary Book ", Category="Books", Price = 21.99M },
+           new Product { Id = 3 , Name = "Blender", Category="Applicance", Price = 60.99M },
+
+        };
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return products;
+        }
+    }
+}
